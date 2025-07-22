fac72807f27969397fd24f874454271657e87ea5
TudorOO <tudormihalcioiu@gmail.com>
2025-07-22 21:36:01 +0300

    a

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import os
 from dotenv import load_dotenv
 
 app = create_app()
-load_dotenv
+load_dotenv()
 
 if __name__ == "__main__":
     print("Starting server...")
-    app.run(host="0.0.0.0", port=os.getenv("PORT"), debug=True)
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT")), debug=True)
